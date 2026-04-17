9f833cba2db55d10c117b96d5d7d1cdc5352149b
英文名 <>
2026-04-17 10:00:49 +0800

    123

---
+++ test.js
@@ -0,0 +1,11 @@
+// I am a Comment. I do Nothing
+
+// How to Declare variables:
+let x = 5;
+const y = 6; 
+// y = 10; 會出現錯誤
+// How to Compute values:
+let z = x + y;
+
+// How to Output values:
+console.log("hello world");
